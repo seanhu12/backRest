8fae5d1f945db580ad0244d21e738a7f743cb8e5
seanhu12 <sebastian.huanca@alumnos.ucn.cl>
2022-12-08 23:51:36 -0300

    cuarto avance

---
+++ app/Http/Controllers/ImageUploadController.php
@@ -0,0 +1,25 @@
+<?php
+namespace App\Http\Controllers;
+
+use App\Models\Image;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\Rules\File;
+
+class ImageUploadController extends Controller
+{
+    public function uploadImage(Request $request)
+    {
+        $validator = Validator::make($request->all(), ['image' => ['required', File::image()->max(10 * 1024)]]);
+        if ($validator->fails()) return response()->json($validator->messages());
+        $image = new Image();
+        $file = $request->file('image');
+        $filename = uniqid() . "_" . $file->getClientOriginalName();
+        $file->move(public_path('public/images'), $filename);
+        $url = URL::to('/') . '/public/images/' . $filename;
+        $image['url'] = $url;
+        $image->save();
+        return response()->json(['isSuccess' => true, 'url' => $url]);
+    }
+}
